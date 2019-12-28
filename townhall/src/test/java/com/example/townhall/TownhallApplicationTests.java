2bae05f3dd4c6ed6bae42b54dab1dc7cfd6319e4
Djougounah Arnaud <djougounarnaud@gmail.com>
2019-12-28 13:12:57 +0100

    add files projects

---
+++ townhall/src/test/java/com/example/townhall/TownhallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.townhall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownhallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
